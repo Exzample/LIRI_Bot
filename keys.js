d0f6697bf1487a0479f06dab36b3cd509b885c79
Doug Admin <dougadmin28@DatafileTechnologies.local>
2019-05-07 15:28:27 -0500

    adding in pinnnacle.js, need to get user name for live lines

--- keys.js
+++ keys.js
@@ -2,3 +2,7 @@ exports.spotify = {
     id: process.env.SPOTIFY_ID,
     secret: process.env.SPOTIFY_SECRET
 };
+exports.pinnacle = {
+  username: process.env.PINNACLE_USER,
+  Authorization: process.env.PINNACLE_AUTH
+};
